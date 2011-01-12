9a2c53d57b6cf0d30fd6defe1e669ed892157bd2
T S <YenTheFirst@gmail.com>
2011-01-12 14:17:26 -0800

    fixed the datetime, added the georss definition thingy, actually inserted the georss:points missing from the last commit

--- simple.rb
+++ simple.rb
@@ -7,7 +7,7 @@ require 'xmlsimple'
 $server="mayors24.cityofboston.gov"
 $port="4443"
 $base_link="/open311/v2/requests.xml?jurisdiction_id=cityofboston.gov&status=opened&service_code=unshoveled-sidewalk-report"
-
+$rfc822="%a, %d %b %Y %H:%M:%S %Z"
 get '/rss.xml' do
 	http=Net::HTTP.new($server,$port)
 	http.use_ssl=true
@@ -19,7 +19,7 @@ def output_rss(the_hash)
 	builder do |xml|
 		xml.instruct! :xml, :version => '1.0'
 		xml.rss :version => "2.0" do
-			xml.channel do
+			xml.channel "xmlns:georss"=>"http://www.georss.org/georss" do
 				xml.title "Boston Snow georsss"
 				xml.description "cityofboston open311 unshoveled snow"
 				xml.link "https://#{$server}:#{$port}#{$base_link}"
@@ -29,10 +29,12 @@ def output_rss(the_hash)
 					
 					xml.item do
 						xml.title descrip
-						xml.link $server+ticket["media_url"].to_s
+						xml.link "https://#{$server}#{ticket["media_url"]}"
 						xml.description  descrip
-						xml.pubDate ticket["requested_datetime"].to_s
+						
+						xml.pubDate ((Time.parse(ticket["requested_datetime"][0]) rescue Time.now).strftime($rfc822))
 						xml.guid ticket["token"].to_s
+						xml.tag!("georss:point",(ticket["lat"].to_s+" "+ticket["long"].to_s))
 					end
 				end
 			end
